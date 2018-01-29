bc8d280299e2b84ddea0123fb8c1df028d414396
Emma Roberts <emrsixtyseven@gmail.com>
2018-01-29 09:14:53 +0000

    pet shop hw

---
+++ pet_shop/pet_shop.rb
@@ -0,0 +1,130 @@
+def pet_shop_name(name)
+  return name[:name]
+
+end
+
+
+def total_cash(petshop)
+  return petshop[:admin][:total_cash]
+
+end
+
+
+def add_or_remove_cash(cash, amount)
+  cash [:admin][:total_cash] += amount
+  return cash
+end
+
+
+def pets_sold(sold)
+  return sold[:admin][:pets_sold]
+
+end
+
+def increase_pets_sold(sold, amount)
+  sold [:admin][:pets_sold] += amount
+  return sold
+end
+
+def stock_count (pets)
+
+  return pets [:pets].count
+
+end
+
+
+def pets_by_breed(pet_shop, breed)
+  # empty array
+  total_breed = []
+  # loop over pets in shop
+  for pet in pet_shop[:pets]
+    # add pet to array if pet's breed matches breed passed in
+    if pet[:breed] == breed
+      total_breed.push(pet)
+    end
+    # return array
+  end
+  return total_breed
+end
+
+
+def find_pet_by_name(pet_shop, name)
+
+  for pet in pet_shop[:pets]
+    if pet[:name] == name
+
+    return pet
+
+    end
+  end
+
+
+end
+
+
+
+def find_pet_by_name(pet_shop, name)
+  for pet in pet_shop[:pets]
+    if pet[:name] == name
+      return pet
+    end
+  end
+
+  return nil
+
+end
+
+
+# def remove_pet_by_name(pet_shop, name)
+#
+#   for pet in pet_shop[:pets]
+#     if pet[:name] == name
+#       pet[:name].delete(name)
+#       return nil
+#     end
+#   end
+#
+#
+# end
+
+def add_pet_to_stock(pets, new_pet)
+
+  pets[:pets] << new_pet
+
+  return pets[:pets].count
+
+end
+
+
+def customer_pet_count(pets)
+  pet_total = []
+for number in pets[:pets]
+  pet_total << [:pets]
+end
+  return pet_total.count
+
+end
+
+
+def add_pet_to_customer(pets, new_pet)
+  pet_total = []
+  pets[:pets] << new_pet
+for number in pets[:pets]
+
+  pet_total << [:pets]
+
+  return pet_total.count
+end
+end
+
+
+def customer_can_afford_pet(cost, cash)
+
+if cost[:price].to_i < cash[:cash].to_i
+  return true
+
+else
+  false
+end
+
+end
